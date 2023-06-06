bb6ce40683be1129b3d9c0ab88a48ea87a2f313a
FrancoisCharvet <75119685+FrancoisCharvet@users.noreply.github.com>
2023-06-06 09:28:12 +0200

    Single execution working file
    No commands to input by hand
    Everything working with this single file when vagrant up for the first time

--- jenkins.sh
+++ jenkins.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 echo "Adding apt-keys"
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
 echo "Updating apt-get"
-sudo apt-get -qq update
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get upgrade -y
 
-echo "Installing default-java"
-sudo apt-get -y install default-jre 
-sudo apt-get -y install default-jdk 
+echo "Installing java"
+sudo apt-get install -y openjdk-11-jre openjdk-11-jdk
+java -version
 
 echo "Installing git"
-sudo apt-get -y install git 
+sudo apt-get -y install git
 
 echo "Installing git-ftp"
-sudo apt-get -y install git-ftp 
+sudo apt-get -y install git-ftp
+
+echo "Installing node js"
+sudo apt-get install -y nodejs
 
 echo "Installing jenkins"
-sudo apt-get -y install jenkins > 
+sudo apt-get install -y --allow-unauthenticated jenkins
 sudo service jenkins start
 
-sleep 1m
-
-echo "Installing Jenkins Plugins"
+echo "Password"
 JENKINSPWD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 echo $JENKINSPWD
