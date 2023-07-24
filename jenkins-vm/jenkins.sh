0583564feffda4c1b69a6f672770afa74853de82
pierrebonnet78 <60656840+pierrebonnet78@users.noreply.github.com>
2023-07-24 15:27:52 +0200

    test

--- jenkins-vm/jenkins.sh
+++ jenkins-vm/jenkins.sh
@@ -1,70 +1,83 @@
 #!/bin/bash
+echo "Adding apt-keys"
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-#"Adding apt-keys"
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
-# "Updating apt-get"
+echo "Updating apt"
 sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade
 
-#  "Installing java"
-sudo apt install -y openjdk-11-jre
+echo "Installing java"
+sudo apt install -y openjdk-17-jre
 java -version
 
-# "Installing git"
-sudo apt-get -y install git
+echo "Installing git"
+sudo apt install -y git
 
-# "Installing git-ftp"
-sudo apt-get -y install git-ftp
+echo "Installing git-ftp"
+sudo apt install -y git-ftp
 
-# "Installing nodejs and npm"
+echo "Installing nodejs and npm"
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt install -y nodejs
 
-# "Installing webhookRelay and log into" 
+echo "Installing webhookRelay and log into" 
 curl https://my.webhookrelay.com/webhookrelay/downloads/install-cli.sh | bash
 relay login -k e6ebbe7b-ce2c-4871-9b8b-44bf0a87ca44 -s QNvQI8oRpDK2
 
-# "Installing jenkins"
-sudo apt-get install -y --allow-unauthenticated jenkins
+echo "Installing jenkins"
+sudo apt install -y --allow-unauthenticated jenkins
 sudo service jenkins start
 
-# "Installing Jenkins Config as Code plugin"
-sudo curl -fsSLo /var/lib/jenkins/plugins/configuration-as-code.hpi https://updates.jenkins.io/download/plugins/configuration-as-code/1647.ve39ca_b_829b_42/configuration-as-code.hpi
-## to finish 
 
-# "Installing Jenkins Plugin Manager Tool"
+echo #Copy JCasC config file into vm
+cp /vagrant/jenkins.yaml /var/lib/jenkins/jenkins.yaml
+
+echo #Copy pipeline script into vm
+cp /vagrant/seedjob.groovy /usr/local/seedjob.groovy
+
+
+echo "Installing Jenkins Config as Code plugin"
+sudo curl -fsSLo /var/lib/jenkins/plugins/configuration-as-code.hpi https://updates.jenkins.io/download/plugins/configuration-as-code/1647.ve39ca_b_829b_42/configuration-as-code.hpi
+
+
+echo "Installing Jenkins Plugin Manager Tool"
 sudo mkdir -p tools
 sudo wget https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.12.13/jenkins-plugin-manager-2.12.13.jar
-sudo java -jar jenkins-plugin-manager-2.12.13.jar --plugin-download-directory /var/lib/jenkins/plugins --plugins credentials-binding  timestamper ws-cleanup workflow-aggregator github-branch-source git ssh-slaves
+sudo java -jar jenkins-plugin-manager-2.12.13.jar --plugin-download-directory /var/lib/jenkins/plugins --plugins credentials-binding  timestamper ws-cleanup workflow-aggregator github-branch-source git ssh-slaves matrix-auth configuration-as-code docker-plugin job-dsl docker-workflow
 
-#Disable setup wizard at first jenkins launch
-## to finish
-# echo "JAVA_ARGS=\"-Djenkins.install.runSetupWizard=false\"" >> /etc/default/jenkins
+echo "export JENKINS_ADMIN_ID='jenkins'" > /etc/profile.d/myvar.sh
+echo "export JENKINS_ADMIN_PASSWORD='password'" >> /etc/profile.d/myvar.sh
 
-# cd /var/lib/jenkins/
-# sudo mkdir -p init.groovy.d
-# cd init.groovy.d
 
-# code="#!groovy
-# import jenkins.model.*
-# import hudson.util.*;
-# import jenkins.install.*;
-# def instance = Jenkins.getInstance()
-# instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)"
+sed -i 's|^\(JAVA_ARGS="-Djava.awt.headless=true"\)$|JAVA_ARGS="-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false"|' /etc/default/jenkins
+
+cd /var/lib/jenkins/
+sudo mkdir -p init.groovy.d
+cd init.groovy.d
+
+code="#!groovy
+import jenkins.model.*
+import hudson.util.*;
+import jenkins.install.*;
+
+def instance = Jenkins.getInstance()
+
+instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)"
+
 
 # Create the file and add the code
-# echo "$code" > /var/lib/jenkins/init.groovy.d/basic-security.groovy
-
+echo "$code" > /var/lib/jenkins/init.groovy.d/basic-security.groovy
 sudo systemctl restart jenkins
+
 
 echo "Password"
 JENKINSPWD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 echo $JENKINSPWD
 
-#Listen to github webhook call
 relay forward --bucket github-jenkins
 
 #/var/lib/jenkins/
