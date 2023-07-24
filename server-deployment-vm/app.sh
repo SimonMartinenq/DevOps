ccb4d4d15b0a77293a93b02b558c1ae224e44290
pierrebonnet78 <60656840+pierrebonnet78@users.noreply.github.com>
2023-07-24 16:28:03 +0200

    docker-compose file for the server deployment vm

--- server-deployment-vm/app.sh
+++ server-deployment-vm/app.sh
@@ -7,6 +7,8 @@ sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
+cp /vagrant/docker-compose.yml /home/jenkins/docker-compose.yml
+
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
